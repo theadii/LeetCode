0b6f680fd68eb66f22131404d9fe9b33331acf86
theadii <145575458+theadii@users.noreply.github.com>
2026-08-26 17:29:09 +0530

    Time: 0 ms (100%), Space: 43 MB (18.51%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,22 @@
 class Solution {
 
-    int solve(int i,int j,int[] arr){
+    int solve(int i,int j,int[] arr,int[][] dp){
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
         if(i==j){
             return arr[i];
         }
 
-        return Math.max(arr[i] - solve(i+1,j,arr), arr[j] - solve(i,j-1,arr));
+        return dp[i][j] = Math.max(arr[i] - solve(i+1,j,arr,dp), arr[j] - solve(i,j-1,arr,dp));
     }
     public boolean predictTheWinner(int[] nums) {
-        return solve(0,nums.length - 1,nums) >=0;
+        int n = nums.length;
+
+        int dp[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(0,nums.length - 1,nums,dp) >=0;
     }
 }
