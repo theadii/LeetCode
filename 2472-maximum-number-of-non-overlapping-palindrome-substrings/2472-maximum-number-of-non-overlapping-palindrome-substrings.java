46061f3243ad2054a7ebf62ff3e477d3213401d0
theadii <145575458+theadii@users.noreply.github.com>
2026-09-16 01:32:05 +0530

    Time: 2328 ms (5.05%), Space: 250.3 MB (5.05%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,6 +1,7 @@
 class Solution {
 
     int dp[][];
+    boolean check[][];
     boolean isP(int i,int j,String s){
         while(i<=j){
             if(s.charAt(i)!=s.charAt(j)){
@@ -19,14 +20,15 @@ class Solution {
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(isP(i,j,s)){
+        if(check[i][j] || isP(i,j,s)){
+            check[i][j] = true;
             int take = 1 + helper(j+1,j+k,k,s);
             int grow = helper(i,j+1,k,s);
             int slide = helper(i+1,j+1,k,s);
             return dp[i][j] = Math.max(grow,Math.max(slide,take));
         }
         else{
-
+            check[i][j] = false;
             int grow = helper(i,j+1,k,s);
             int slide = helper(i+1,j+1,k,s);
 
@@ -37,6 +39,7 @@ class Solution {
     public int maxPalindromes(String s, int k) {
         int n = s.length();
         dp = new int[n+1][n+1];
+        check = new boolean[n+1][n+1];
         if(k==1) return n;
         for(int i=0;i<=n;i++){
             Arrays.fill(dp[i],-1);
