605b111f9eab3c824114cb44f196a9239eec56bb
theadii <145575458+theadii@users.noreply.github.com>
2026-09-19 22:53:16 +0530

    Time: 0 ms (100%), Space: 43.2 MB (42.68%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -1,33 +1,19 @@
 class Solution {
-
-    boolean check(int arr[],int i,int j){
-        int diff = arr[i+1] - arr[i];
-
-        for(int k=i+1;k<=j;k++){
-            if(arr[k] - arr[k-1] != diff){
-                return false;
-            }
-        }
-        return true;
-    }
     public int numberOfArithmeticSlices(int[] nums) {
-        int ans = 0;
 
-        int n = nums.length;
-
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-
-                if(j-i+1 >=3){
-                    if(check(nums,i,j)){
-                        ans++;
-                    }
-                }
-
-
+        if(nums.length<3) return 0;
+        int res=0;
+        int count=0;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
+            { 
+                count++;
+                res+=count;
+            }
+            else{
+                count=0;
             }
         }
-
-        return ans;
+        return res;
     }
 }
